e4e3eace9e0a96c28d5db3eb0498c7eaa0dcd36c
Jay Vigilla <jvigilla@avail.io>
2024-01-17 17:13:35 -0800

    WIP metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 // import {useState} from 'react'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -6,10 +6,10 @@ import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
-// export const metadata: Metadata = {
-  //   title: 'Jay Vigilla | Software Engineer',
-  //   description: 'Jay Vigilla Software Engineer in San Francisco Bay Area',
-  // }
+export const metadata: Metadata = {
+    title: 'Jay Vigilla | Software Engineer | San Francisco',
+    description: 'Jay Vigilla is a Software Engineer based in the San Francisco Bay Area experienced in JavaScript, Typescript, React (Hooks/Classes), Redux (Sagas/Thunks), NodeJS, Python, SQL, and responsive design',
+  }
 
   export default function RootLayout({
     children,
