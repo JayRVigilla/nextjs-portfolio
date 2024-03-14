74143302d7cfca51297a9305f410075313c97ec7
Jay Vigilla <jvigilla@avail.io>
2024-03-14 13:04:04 -0700

    clean

--- app/components/Navigation/content.js
+++
@@ -1,7 +0,0 @@
-export const pages = [
-  { href: "/", name: "Home" },
-  { href: "/projects", name: "Projects" },
-  { href: "/about", name: "About" },
-  { href: "http://babbledotlog.jayvigilla.com", name: "Blog", target: "_blank" },
-  // { href: "/contact", name: "Contact" },
-]
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import styles from './page.module.css'
 import "./Home.css"
 import { BeamIn } from './components/BeamIn';
 import { Jobs } from './components/Jobs';
-// import Projects from './projects/page';
 import { Projects } from './components/Projects';
 import { About } from './components/About';
 export default function Main() {
