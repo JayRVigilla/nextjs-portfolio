e702be0303d6d0118d2bcee44ebc76c868ffa085
Jay Vigilla <jvigilla@avail.io>
2024-03-14 13:13:39 -0700

    clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from 'next'
-// import {useState} from 'react'
-// import { League_Spartan } from 'next/font/google'
-import { League_Spartan, Roboto_Mono } from 'next/font/google'
+import { Roboto_Mono } from 'next/font/google'
 import './globals.css'
-// import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 
-// const inter = Open_Sans({ subsets: ['latin'] }) // * Like
 const inter = Roboto_Mono({ subsets: ['latin'] }) // * Like
-// const inter = Roboto_Slab({ subsets: ['latin'] }) // * Like
-// const inter = Montserrat({ subsets: ['latin'] }) // * Like
-// const inter = League_Spartan({ subsets: ['latin'] }) // * Like for headings
-// const inter = League_Spartan({ subsets: ['latin'] }) // * Like for headings
-// const inter = Titillium_Web({ subsets: ['latin'], weight: "300" }) // * Like for text
 export const metadata: Metadata = {
     title: 'Jay Vigilla | Software Engineer | San Francisco',
-    description: 'Jay Vigilla is a Software Engineer based in the San Francisco Bay Area experienced in JavaScript, Typescript, React (Hooks/Classes), Redux (Sagas/Thunks), NodeJS, Python, SQL, and responsive design',
+    description: 'Jay Vigilla is a chef turned Software Engineer based in the San Francisco Bay Area experienced in JavaScript, Typescript, React, Redux, NodeJS, Python, SQL, responsive design, and team management under pressure',
   }
 
   export default function RootLayout({
