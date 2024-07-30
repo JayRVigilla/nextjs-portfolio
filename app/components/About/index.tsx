814eff2078657a143e53732741f121aef2d95bc4
Jay Vigilla <jayrvigilla@gmail.com>
2024-07-29 21:22:51 -0700

    quick clean up

--- app/components/About/index.tsx
+++ app/components/About/index.tsx
@@ -4,7 +4,7 @@ export const About = () => {
       <h2>{`Tech from Hospitality`}</h2>
       <p>{` Hi! I'm Jay, a former chef in the San Francisco Bay Area that changed careers to Software Engineering. I make technology giving people the same feeling of care and visibility that the Hospitality Industry instilled in me.`}</p>
 
-      <p>{`As a kid, I was a tinkerer. I took things apart and put them back together, slowing understanding what things did and hiding the extra pieces from my parents. Playing with technology and building things is my favorite part of the job. As with my food career, I like hands-on experience.`}</p>
+      <p>{`As a kid, I was a tinkerer. I took things apart and put them back together, slowly understanding what things did and hiding the extra pieces from my parents. Playing with technology and building things is my favorite part of the job. As with my food career, I like hands-on experiences.`}</p>
 
       <p>{`For more about the Hospitality Industry and Technology overlap (in my head at least) check out my blog:`}<a href="https://babbledotlog.jayvigilla.com/" target="_blank" rel="noreferrer"> babble.log()</a></p>
     </section>
