63762b51b0d9fdd6011cd220d1af3f90cce53bdc
Jay Vigilla <jayrvigilla@gmail.com>
2024-08-29 09:44:00 -0700

    sanity save

--- app/components/About/index.tsx
+++ app/components/About/index.tsx
@@ -2,9 +2,9 @@ export const About = () => {
   return (
     <section className="teaser about">
       <h2>{`About Me`}</h2>
-      <p>{` Hi! I'm Jay, a Front End Engineer the San Francisco Bay Area. I am a former chef that reveled in learning how smaller systems work together to create intricate infrastructures. I've coupled with my knack for high-level operational thinking with an ability to communicate with across team domains on the fly.`}</p>
+      <p>{` Hi! I'm Jay, a Front End Engineer the San Francisco Bay Area. I am a former chef that reveled in learning how smaller systems work together to create intricate infrastructures. I've coupled my knack for high-level operational thinking with an ability to communicate with across team domains on the fly.`}</p>
 
-      <p>{`As a kid, I was a tinkerer. I took things apart and put them back together, slowly understanding what things did and hiding the extra pieces from my parents. Playing with technology and building things is my favorite part of the job. As with my food career, I like hands-on experiences.`}</p>
+      <p>{`As a kid, I was a tinkerer. I took things apart and put them back together. Luckily, my parents were patient as I reassembled parts of the stereo. I took the time to understand what things did and hiding the extra pieces from my parents. Playing with technology and building things is my favorite part of the job. As with my food career, I like hands-on experiences.`}</p>
 
     </section>
   )
